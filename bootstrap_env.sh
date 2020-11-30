4728d074e490d7aa99cc4853afd1bdec1bc40b74
Maulik Shah <shahmaulikb@gmail.com>
2020-11-30 00:17:09 -0800

    new client v2 demo

--- bootstrap_env.sh
+++
@@ -1,7 +0,0 @@
-pip install -r requirements.txt
-rm -rf ~/.marlin/config
-mkdir ~/.marlin
-echo "[default]" > ~/.marlin/config
-echo "client_id: $(date +%s)_" >> ~/.marlin/config
-
-echo "------------- Bootstrap successful -------------"
